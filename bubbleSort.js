6b883666eaf9b5495e8feba3de7a6fad705b2f0d
Erfan Ansari <dev.erfanansari@gmail.com>
2022-03-18 13:14:49 +0330

    feat: update update bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,4 +1,6 @@
 function bubbleSort(arr) {
+    // The nowSwaps variable is a performance optimization to avoid the need to
+    // check on every iteration if the array is already sorted
     let nowSwaps = false
     for (let i = arr.length; i > 0; i--) {
         nowSwaps = true
@@ -15,3 +17,8 @@ function bubbleSort(arr) {
 }
 
 console.log(bubbleSort([3, 2, 6, 30, 49, 57, 60])) // [2, 3, 6, 30, 49, 57, 60]
+
+// Not that all efficient , Not commonly used but in one use case it does excel
+// Why it's called bubble sort(syncing sort)? Because it's a sorting algorithm where the largest values bubble up to the top!
+// Time Complexity: O(n^2)
+// Best Case: O(n)
