d176b1e0183893d1ab1c4482e8a0fbcc450d95b4
Erfan Ansari <dev.erfanansari@gmail.com>
2022-11-12 16:17:20 +0330

    add ReturnOf.ts equelevent to core utility ReturnType

---
+++ type-challenges/ReturnOf.ts
@@ -0,0 +1,55 @@
+// @errors: 2344
+type Expect<T extends true> = T
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+    ? 1
+    : 2
+    ? true
+    : false
+
+type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true
+
+// ---cut---
+
+// Implement this type
+type ReturnOf<F extends (...args: any[]) => void> = F extends {
+    (...args: any[]): infer RT
+}
+    ? RT
+    : never
+
+// Tests
+
+const flipCoin = () => (Math.random() > 0.5 ? 'heads' : 'tails')
+const rockPaperScissors = (arg: 1 | 2 | 3) => {
+    return arg === 1
+        ? ('rock' as const)
+        : arg === 2
+        ? ('paper' as const)
+        : ('scissors' as const)
+}
+
+type cases = [
+    // simple 1
+    Expect<Equal<boolean, ReturnOf<() => boolean>>>,
+    // simple 2
+    Expect<Equal<123, ReturnOf<() => 123>>>,
+    Expect<Equal<ComplexObject, ReturnOf<() => ComplexObject>>>,
+    Expect<Equal<Promise<boolean>, ReturnOf<() => Promise<boolean>>>>,
+    Expect<Equal<() => 'foo', ReturnOf<() => () => 'foo'>>>,
+    Expect<Equal<'heads' | 'tails', ReturnOf<typeof flipCoin>>>,
+    Expect<
+        Equal<'rock' | 'paper' | 'scissors', ReturnOf<typeof rockPaperScissors>>
+    >,
+    Expect<
+        Equal<
+            'rock' | 'paper' | 'scissors',
+            ReturnType<typeof rockPaperScissors>
+        >
+    >,
+]
+
+type ComplexObject = {
+    a: [12, 'foo']
+    bar: 'hello'
+    prev(): number
+}
